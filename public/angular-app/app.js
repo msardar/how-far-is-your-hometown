c39f38e6e24b99e40a877171efae66a1b4916aee
ecmtest <mmalik@microsoft.com>
2017-05-15 20:30:39 -0500

    Fix the ng-view error
    Decide the approach to show the result only when the Go! button is clicked

--- public/angular-app/app.js
+++ public/angular-app/app.js
@@ -3,7 +3,7 @@ angular.module('hometownApp', ['ngRoute']).config(config)
 
 function config($routeProvider) {
     $routeProvider
-        .when('/', {
+        .when('/result', {
             templateUrl: 'angular-app/hometown-address/hometown-result.html',
             controller: HomeAddressController,
             controllerAs: 'vm'
